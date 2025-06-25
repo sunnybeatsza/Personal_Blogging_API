712351d4cd172ed83ce3e6031bd7824f7460db43
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2025-06-25 21:32:11 +0200

    added the spring set up and created the basic class

---
+++ Personal_blog/src/main/java/com/example/Personal_blog/Articles/Article.java
@@ -0,0 +1,10 @@
+package com.example.Personal_blog.Articles;
+
+import java.util.ArrayList;
+
+public class Article {
+    private int articleId;
+    public String articleAuthor;
+    public int articleYear;
+    public ArrayList<String> articleTags;
+}
